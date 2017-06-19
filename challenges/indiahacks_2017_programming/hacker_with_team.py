ef98939d5dea58b1eff8e6a35977f3d73759e385
Runcy Oommen <runcy.oommen@gmail.com>
2017-06-19 18:03:43 +0530

    Incomplete solutions to challenges 1 and 2

--- challenges/indiahacks_2017_programming/hacker_with_team.py
+++ challenges/indiahacks_2017_programming/hacker_with_team.py
@@ -5,6 +5,17 @@ for _ in range(Q):
     inp = [int(q) for q in input().split(' ')]
     O = inp[0]
     if O == 1:
+        # print("S is", S)
         l, r, K = inp[1], inp[2], inp[3]
+        skills = 0
+        for members in range(l-1, r):
+            # print("range", S[(members-K):(members+1)])
+            sum_ = sum(s for s in S[(members-K):(members+1)])
+            skills += sum_
+        print(skills)
     if O == 2:
         i, X = inp[1], inp[2]
+        S[i-1] = X
+        # print(S)
+
+
